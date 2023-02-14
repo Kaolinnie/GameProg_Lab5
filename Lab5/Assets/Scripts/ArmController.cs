959069ad58fb94368aa0abeb3979e793b41b025e
Kaolin <kaolin.stacey@gmail.com>
2023-02-13 20:30:18 -0500

    did most

---
+++ Lab5/Assets/Scripts/ArmController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour {
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            animator.SetBool("ShouldThrowBall",true);
+        }
+    }
+
+    
+    void ReleaseBall()
+    {   
+        BallController ball = GetComponentInChildren<BallController>();
+        if (ball != null)
+        {
+            ball.transform.parent = null;
+            ball.LaunchMe();
+        };
+    }
+}
