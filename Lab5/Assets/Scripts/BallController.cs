eb12b7e0f57e563f8980b54bb279d69b97c31459
Kaolin <kaolin.stacey@gmail.com>
2023-02-14 08:17:36 -0500

    Lab5 done

--- Lab5/Assets/Scripts/BallController.cs
+++ Lab5/Assets/Scripts/BallController.cs
@@ -7,15 +7,20 @@ public class BallController : MonoBehaviour
 {
     
     public GameObject bowlingPin;
+    public Rigidbody rb;
+    public float strength = 10f;
+
     public void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     public void LaunchMe()
     {   
         // TO DO 
         // Write code to launch ball
+        rb.isKinematic = false;
+        rb.AddForce(GetLaunchDirection()*strength,ForceMode.Impulse);
     }
 
     public Vector3 GetLaunchDirection()
